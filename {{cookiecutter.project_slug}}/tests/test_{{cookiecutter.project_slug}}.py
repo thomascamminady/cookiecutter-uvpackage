80f58342f1a54c61c9d459149874a9a518b515c5
Thomas <0milieux_member@icloud.com>
2023-03-02 20:13:44 +0100

    Update test_{{cookiecutter.project_slug}}.py

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -2,12 +2,7 @@
 
 
 from {{ cookiecutter.project_slug }} import __version__
-from {{ cookiecutter.project_slug }}.add1 import add1
 
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
-
-
-def test_add1() -> None:
-    assert add1(2) == 3
