5c427aab16f6e0225874f60a41e109253252e109
Thomas Camminady <0milieux_member@icloud.com>
2023-03-23 17:18:12 +0100

    Add logger and config.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,10 @@
 """Top-level package for {{ cookiecutter.project_name }}."""
+from {{ cookiecutter.project_name }}.utils.config import config
+from {{ cookiecutter.project_name }}.utils.logger import logger
 
+__all__ = ["config", "logger"]
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = "{{ cookiecutter.email }}"
 __version__ = "{{ cookiecutter.version }}"
+
+
