8d953be5a7598c34002289fe154470c5c87fa366
thomas <0milieux_member@icloud.com>
2024-05-09 07:30:24 +0200

    Update test_{{cookiecutter.project_slug}}.py

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,8 +1,5 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
 
-from {{cookiecutter.project_slug}} import __version__
-
-
-def test_version() -> None:
-    assert __version__ == "0.1.0"
+def test_true() -> None:
+    assert True
