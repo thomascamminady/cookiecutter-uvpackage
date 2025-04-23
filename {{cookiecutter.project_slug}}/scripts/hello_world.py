bc13d68b9f6ef0a02eb8286a2b8fa50b93fd8593
Thomas Camminady <0milieux_member@icloud.com>
2025-04-23 09:25:36 +0200

    Adding

---
+++ {{cookiecutter.project_slug}}/scripts/hello_world.py
@@ -0,0 +1,11 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#   "rich",
+# ]
+# ///
+
+from rich import print
+
+if __name__ == "__main__":
+    print("Hello, World!")
--- {{cookiecutter.project_slug}}/tests/test_true.py
+++ {{cookiecutter.project_slug}}/tests/test_true.py
@@ -1,6 +1,3 @@
-"""Tests for `{{ cookiecutter.project_slug }}` package."""
-
-
 def test_true() -> None:
     """At least one test will always pass :)."""
     assert True
