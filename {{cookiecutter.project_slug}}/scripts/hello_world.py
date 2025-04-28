e833f48ba12c684f401ef87cc0d0c452d7b2accd
thomas <0milieux_member@icloud.com>
2025-04-28 11:03:20 +0200

    Update hello_world.py

--- {{cookiecutter.project_slug}}/scripts/hello_world.py
+++ {{cookiecutter.project_slug}}/scripts/hello_world.py
@@ -12,7 +12,7 @@ from rich import print
 
 
 def main():
-    print("Run via `uv run hello_world.py`")
+    print("Run via `uv run scripts/hello_world.py`")
     print("Or `chmod +x scripts/hello_world.py` and `./scripts/hello_world.py`")
 
 
