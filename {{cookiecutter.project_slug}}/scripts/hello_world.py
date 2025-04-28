8ca37c996aacb4a1257f49e1bdb0e6b8b927d1bb
thomas <0milieux_member@icloud.com>
2025-04-28 10:55:44 +0200

    Update hello_world.py

--- {{cookiecutter.project_slug}}/scripts/hello_world.py
+++ {{cookiecutter.project_slug}}/scripts/hello_world.py
@@ -4,8 +4,13 @@
 #   "rich",
 # ]
 # ///
+# pyright: reportMissingModuleSource=false
+# pyright: reportMissingImports=false
 
 from rich import print
 
-if __name__ == "__main__":
+def main():
     print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
