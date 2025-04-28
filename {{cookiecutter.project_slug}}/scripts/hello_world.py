bc735713a254c967261b2c9c79ed9183c25463c9
thomas <0milieux_member@icloud.com>
2025-04-28 11:02:50 +0200

    Update hello_world.py

--- {{cookiecutter.project_slug}}/scripts/hello_world.py
+++ {{cookiecutter.project_slug}}/scripts/hello_world.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env -S uv run --script
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
@@ -11,9 +12,9 @@ from rich import print
 
 
 def main():
-    print("Hello, World!")
+    print("Run via `uv run hello_world.py`")
+    print("Or `chmod +x scripts/hello_world.py` and `./scripts/hello_world.py`")
 
 
 if __name__ == "__main__":
     main()
-
